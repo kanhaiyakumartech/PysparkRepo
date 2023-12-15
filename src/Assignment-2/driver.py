4d937c356d9b2b940200c6cc28eaa99c55815e8b
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2023-12-15 21:37:25 +0530

    This is the driver file for credit card data

--- src/Assignment-2/driver.py
+++ src/Assignment-2/driver.py
@@ -1,45 +1,40 @@
 from pyspark.sql import SparkSession
-from utils import mask_card_udf
+from PysparkRepo/src/Assignment2/util import *
 
-# Here I  Create a SparkSession
-spark = SparkSession.builder.appName("CreditCardData").getOrCreate()
-
-# Now I Define the dataset
+# Sample data
 data = [
-    ("1234567891234567",),
-    ("5678912345671234",),
-    ("9123456712345678",),
-    ("1234567812341122",),
-    ("1234567812341342",)
+        ("1234567891234567",),
+        ("5678912345671234",),
+        ("9123456712345678",),
+        ("1234567812341122",),
+        ("1234567812341342",)
 ]
 
-# Define the schema for the DataFrame
-schema = ["card_number"]
+# Create DataFrame
 
-from src/Assignment-2/utils.py import *
-# Create DataFrame using createDataFrame method
-credit_card_df = spark.createDataFrame(data, schema)
+credit_card_df = spark.read.csv('dbfs:/path/src/file.csv', header=False, inferSchema=True).toDF("card_number")
 
-# Show the DataFrame
+# showing the df 
+
 credit_card_df.show()
 
-# Get the number of partitions
-num_partitions = credit_card_df.rdd.getNumPartitions()
-print("Number of partitions:", num_partitions)
+# Print number of partitions
+print("No of partitions before adjustment:", credit_card_df.rdd.getNumPartitions())
 
-# Repartition the DataFrame
-repartitioned_df = credit_card_df.repartition(13)
-repartitioned_partitions = repartitioned_df.rdd.getNumPartitions()
-print("Number of repartitioned partitions:", repartitioned_partitions)
+# Increase the partition size to 5
+credit_card_df = credit_card_df.repartition(5)
 
-# Reduce the partition size back to its original size
-back_to_og_credit_card_df = repartitioned_df.coalesce(num_partitions)
-back_num_partitions = back_to_og_credit_card_df.rdd.getNumPartitions()
-print("Number of back_to_og_partitions:", back_num_partitions)
+# Decrease the partition size back to its original partition size
+credit_card_df = credit_card_df.coalesce(1)
 
-# Apply the UDF to create a new column 'masked_card_number'
-masked_credit_card_df = back_to_og_credit_card_df.withColumn("masked_card_number", mask_card_udf()("card_number"))
+# Save DataFrame to disk
+credit_card_df.write.mode('overwrite').parquet("/tmp/credit_card_data")
 
-# Show the DataFrame with masked card numbers and repartitioned data
-print("DataFrame with masked card numbers and repartitioned data:")
-masked_credit_card_df.show()
+# Read DataFrame back into memory
+credit_card_df = spark.read.parquet("/tmp/credit_card_data")
+
+# Apply UDF to mask card numbers
+credit_card_df = apply_mask_card_udf(credit_card_df)
+
+# Show DataFrame
+credit_card_df.show()
